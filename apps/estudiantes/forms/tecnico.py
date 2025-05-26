502abd7648ca37300798e408e194e754f6a0da7b
ailyn <ailynencinasg@gmail.com>
2025-05-26 15:59:52 -0400

    estiloooooooooos y un poquito de validacion

--- apps/estudiantes/forms/tecnico.py
+++ apps/estudiantes/forms/tecnico.py
@@ -1,5 +1,9 @@
 from django import forms
+from django.core.exceptions import ValidationError  # <-- ESTA LÍNEA ES NECESARIA
 from apps.estudiantes.models.estudiante import Estudiante
+import re
+import os
+
 
 class EstudianteTecnicoForm(forms.ModelForm):
     class Meta:
@@ -9,11 +13,36 @@ class EstudianteTecnicoForm(forms.ModelForm):
             'telefono', 'colegio_universidad', 'ocupacion', 'archivo_documentacion'
         ]
         widgets = {
-            'fecha_nacimiento': forms.DateInput(attrs={'type': 'date'})
+            'nombres': forms.TextInput(attrs={
+                'class': 'block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+            }),
+            'apellidos': forms.TextInput(attrs={
+                'class': 'block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+            }),
+            'ci': forms.TextInput(attrs={
+                'class': 'block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+            }),
+            'fecha_nacimiento': forms.DateInput(attrs={
+                'type': 'date',
+                'class': 'block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+            }),
+            'direccion': forms.TextInput(attrs={
+                'class': 'block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+            }),
+            'telefono': forms.TextInput(attrs={
+                'class': 'block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+            }),
+            'colegio_universidad': forms.TextInput(attrs={
+                'class': 'block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+            }),
+            'ocupacion': forms.TextInput(attrs={
+                'class': 'block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+            }),
+            'archivo_documentacion': forms.ClearableFileInput(attrs={
+                'class': 'block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+            }),
         }
-
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Hacer archivo obligatorio
         self.fields['archivo_documentacion'].required = True
